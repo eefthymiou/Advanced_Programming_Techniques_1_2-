49eafb829ee27cbbf40dbe4f55818c1e09392dfa
eefthymiou <johnefthimiou1002@gmail.com>
2023-05-31 02:29:26 +0300

    subtrahend changed

--- gr.upatras.ask1/src/main/java/gr/upatras/ask1/MyMain.java
+++ gr.upatras.ask1/src/main/java/gr/upatras/ask1/MyMain.java
@@ -3,7 +3,7 @@ package gr.upatras.ask1;
 public class MyMain {
 	public static void main(String[] args) {
 		MyClass m = new MyClass();
-		String message = m.subtraction(10, 5);
+		String message = m.subtraction(15, 5);
 		System.out.println("the message is : " + message);
 	}
 
